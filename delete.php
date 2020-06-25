731921564e048965de0b7eb5a41dfa4229f3513d
ShinyaTanaka46 <s.tanaka.sys19@morijyobi.ac.jp>
2020-06-25 11:32:52 +0900

    commit

---
+++ delete.php
@@ -0,0 +1,24 @@
+<?php
+$dsn = 'mysql:dbname=sample_db;localhost;';
+$user = 'shinya';
+$password = 'morijyobi';
+try {
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    $id = $_GET['id'];
+
+    $sql = "delete from user where id = :id";
+    $stmt = $dbh->prepare($sql);
+    $params = array(':id' => $id);
+
+    $stmt-> execute($params);
+
+    header('Location: index.php?fg=1');
+
+} catch (PDOException $e) {
+    //echo "接続失敗: " . $e->getMessage() . "\n";
+    header('Location: index.php?fg=0?err->getMessage()');
+    exit();
+}
+?>
